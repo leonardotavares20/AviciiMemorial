2527e6f9140e1d3d79c77aa3eb4e79c2f067684c
leonardotavares20 <leo.tav.12345@gmail.com>
2024-06-09 22:42:39 -0300

    init: start of the project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import Header from '../components/Header/Header.svelte';
+	import "../styles/init.sass"
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		
+	</footer>
+</div>
+
+<style>
+	
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Avicii" />
+</svelte:head>
+
+<section>
+</section>
+
+
+<style lang="sass">
+	
+</style>
