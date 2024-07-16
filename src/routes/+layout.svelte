61da5c56c3f8642939fbbdc8dfad8f583e99aea0
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-07-16 19:32:50 -0300

    chore: remove unused imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,10 @@
 <script>
   import Header from "$lib/components/Header/Header.svelte";
-  import ScrollTrigger from "gsap/dist/ScrollTrigger";
-  import ScrollToPlugin from "gsap/dist/ScrollToPlugin";
   import { createSmoothScroll } from "$lib/assets/lenis/lenis";
   import { onMount } from "svelte";
   import "../styles/init.sass";
-  import gsap from "gsap";
 
   onMount(() => {
-    gsap.registerPlugin(ScrollTrigger);
-    gsap.registerPlugin(ScrollToPlugin);
-    
     createSmoothScroll();
   });
 </script>
