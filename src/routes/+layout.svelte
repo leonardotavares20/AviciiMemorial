4b8eeccc1be3e01f97e09e3f11e460d567d72e7e
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-07-16 19:19:01 -0300

    fix: import lenis

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
   import Header from "$lib/components/Header/Header.svelte";
   import ScrollTrigger from "gsap/dist/ScrollTrigger";
   import ScrollToPlugin from "gsap/dist/ScrollToPlugin";
-  import { createSmoothScroll } from "$lib/assets/scroll/smoothScroll";
+  import { createSmoothScroll } from "$lib/assets/lenis/lenis";
   import { onMount } from "svelte";
   import "../styles/init.sass";
   import gsap from "gsap";
