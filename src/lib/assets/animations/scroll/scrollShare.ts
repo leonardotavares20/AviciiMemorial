5ec39e5118830fc936a1462e1bb9d8e4d1b19463
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-07-16 19:03:42 -0300

    chore: remove createScroll function

--- src/lib/assets/animations/scroll/scrollShare.ts
+++ src/lib/assets/animations/scroll/scrollShare.ts
@@ -31,24 +31,3 @@ export class ScrollShare extends BaseScroll {
     };
   }
 }
-
-export function createSmoothScrollShare(onComplete: () => void): ScrollTrigger {
-  return ScrollTrigger.create({
-    trigger: "#share-form__background",
-    once: true,
-    onEnter: () => {
-      gsap.to("#share-form__button", {
-        duration: 1.5,
-        opacity: 1,
-        y: 0,
-        delay: 1,
-      });
-      gsap.to("#share-form__background", {
-        opacity: 1,
-        duration: 2.5,
-        delay: 0.7,
-        onComplete: onComplete,
-      });
-    },
-  });
-}
