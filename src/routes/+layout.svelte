9b859a06532c0f84a095fe7b3865a9127ec09c3e
Leonardo Tavares <leo.tav.12345@gmail.com>
2024-08-08 03:24:29 -0300

    feat: add study case

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
   import "../app.css";
   import Header from "@/components/Header/Header.svelte";
+  import TermsBox from "@/components/Terms/TermsBox.svelte";
   import { createSmoothScroll } from "$lib/assets/lenis/lenis";
   import { onMount } from "svelte";
   import "../styles/init.sass";
@@ -12,16 +13,38 @@
 
 <div class="app">
   <Header></Header>
+  <TermsBox />
 
   <main>
     <slot></slot>
   </main>
 
-  <footer></footer>
+  <footer id="footer" class="footer">
+    This project was created for educational purposes only and as a fan project,
+    I used it only as a study and practice to develop my programming skills. It
+    is not an official project or endorsed by Avicii, Pophouse Entertainment
+    Group AB or any affiliated entities. This is my the Repo of the project: <a
+      class="footer__link-repo"
+      href="https://github.com/leonardotavares20/AviciiMemorial"
+      target="_blank">Repo</a
+    >
+  </footer>
 </div>
 
-<style>
-  .app {
-    overflow: hidden;
-  }
+<style lang="sass">
+  .app
+    margin: auto
+
+  .footer 
+    color: white
+    text-align: center
+    line-height: 25px
+    text-transform: uppercase
+    padding: 30px 200px
+
+  .footer__link-repo
+    text-decoration: underline
+
+  .footer__link-repo:hover 
+    color: #8f8e8d
 </style>
